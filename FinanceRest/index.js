2250fbd6d920430b65dc65df82759a5eb2cd0387
Marcos <markitosanches@gmail.com>
2023-11-24 11:02:36 -0500

    seance 4

--- FinanceRest/index.js
+++ FinanceRest/index.js
@@ -1,6 +1,57 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const request = require('request')
+const config = require('./config')
+const port = config.PORT
+const api_key = config.API_KEY
 
-app.listen(8040, () => {
+
+app.get('/ticker=:ticker', function (req, res) {
+    const ticker = req.params.ticker
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=5min&apikey=${api_key}`;
+    //console.log(url)
+    request.get({
+        url: url,
+        json: true,
+        headers: {'User-Agent': 'request'}
+      }, (err, response, data) => {
+        if (err) {
+          console.log('Error:', err);
+        } else if (response.statusCode !== 200) {
+          console.log('Status:', response.statusCode);
+        } else {
+          // data is successfully parsed as a JSON object:
+          //console.log(data);
+          const newData = JSON.stringify(data)
+          fs.writeFile(`${ticker}.json`, newData, (err) => {
+            if (err) {
+                console.log('File write error', err)
+                res.status(500).send('Internal Server Error')
+            } else {
+                console.log('File write success')
+                res.send('Success')
+            }
+          })
+        }
+    })
+   // res.send('Success')
+})
+
+app.get('/ticker-result=:ticker', function (req, res) {
+    const ticker = req.params.ticker
+    const filePath = `${__dirname}/${ticker}.json`
+
+    fs.readFile(filePath, 'utf8', function(err, data){
+        if(err) {
+            console.log('File read error', err)
+            res.status(500).send('Internal Server Error')
+        } else {
+            res.send(JSON.parse(data))
+        }
+    })
+})
+
+app.listen(port || 5000, () => {
     console.log('Running ...')
 })
--- REST/index.js
+++ REST/index.js
@@ -17,7 +17,7 @@ app.get('/listUsers', function (req, res) {
 app.get('/form', function(req, res){
     res.sendFile(__dirname + "/form.html")
 })
-
+//insert
 app.post('/add-user', function(req, res){
    // console.log(req.body.id)
    fs.readFile(__dirname +"/user.json", "utf8", function(err, data) {
@@ -32,6 +32,56 @@ app.post('/add-user', function(req, res){
         })
    })
 })
+
+//show
+app.get('/form-data', function(req, res){
+    res.sendFile(__dirname + "/form-data.html")
+})
+
+//get id
+///id=10&name=PETER
+app.get('/id=:id', function(req, res) {
+    fs.readFile(__dirname + "/user.json", "utf8", function (err, data){
+        if(err) throw err
+        let users = JSON.parse(data)
+        let user = users["user"+req.params.id]
+        console.log(user)
+        res.end(JSON.stringify(user))
+    })
+})
+
+//update 
+app.put('/update-user', function(req, res){
+    //console.log(req.body)
+    fs.readFile(__dirname + "/user.json", "utf8", function(err, data){
+        if(err) throw err
+        data = JSON.parse(data)
+        data["user"+req.body.id] = req.body
+        let newData = JSON.stringify(data)
+        fs.writeFile(__dirname + "/user.json", newData, err => {
+            if(err) throw err
+            console.log("success")
+        })
+    })
+})
+
+//delete
+app.delete('/delete-user/:id', function(req, res){
+    fs.readFile(__dirname + "/user.json", "utf8", function(err, data){
+        if(err) throw err
+        data = JSON.parse(data)
+        delete data["user"+req.params.id]
+        let newData = JSON.stringify(data)
+        fs.writeFile(__dirname + "/user.json", newData, err => {
+            if(err) throw err
+            console.log("success")
+        })
+    })
+
+})
+
+
+
 
 
 const server = app.listen(8081, function () {
