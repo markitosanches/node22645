07eb2ae95adc97ace6aa828b3f37bb325b095daf
Marcos <markitosanches@gmail.com>
2024-01-15 14:56:53 -0500

    seance 15

---
+++ project-vue/client/src/components/MainFooter.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="w-full mx-auto  border-t border-gray-100 mt-1 p-4 flex items-center justify-center">
+        <span class="text-sm text-gray-500 dark:text-gray-400">All Rights Reserved ©2024 .</span>
+    </footer>
+</template>
